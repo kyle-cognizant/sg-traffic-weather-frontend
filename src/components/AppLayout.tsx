a2405f6d45f9e88813c73aeae3ad37c75cb5016b
Kyle Oliveiro <kyle_oliveiro@techpass.gov.sg>
2023-12-20 08:08:24 +0800

    chore: fix linting

--- src/components/AppLayout.tsx
+++ src/components/AppLayout.tsx
@@ -2,19 +2,19 @@ import { AppShell } from '@mantine/core';
 import { FC } from 'react';
 
 type Props = {
-    children?: React.ReactNode
+  children?: React.ReactNode
 };
 
 const AppLayout: FC<Props> = ({ children }) => {
-    return (
-        <AppShell
-            padding="md"
-        >
-            <AppShell.Main>
-                {children}
-            </AppShell.Main>
-        </AppShell>
-    );
+  return (
+    <AppShell
+      padding="md"
+    >
+      <AppShell.Main>
+        {children}
+      </AppShell.Main>
+    </AppShell>
+  );
 }
 
 export default AppLayout
